7767e62bb63e2ff7aaf7aa3abc766477d812c69b
Seonghun Kim <kim.seonghun23@gmail.com>
2021-08-29 17:47:21 +0900

    Initial Commit

---
+++ WhatIsThisPlants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatIsThisPlants
+//
+//  Created by Seonghun Kim on 2021/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
